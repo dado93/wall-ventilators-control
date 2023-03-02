a31a2ed2b4938b5dc9b2fbc418bfbf8d4f129982
dado93 <davide.marzorati.93@gmail.com>
2023-03-02 22:02:57 +0100

    Added platformio project for IR Control

---
+++ firmware/room-unit/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600, SERIAL_8N1, SERIAL_TX_ONLY);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("NEC");
+  irsend.sendNEC(0x00FFE01FUL);
+  delay(2000);
+}
